3a5c2fd1feed81067e7e72ff85bb6f9bc61fbe89
gagarina6794 <gagarina6794@gmail.com>
2018-08-21 16:28:27 +0300

    HW07 make controllers for Meal and tests + getBetween()

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,81 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import ru.javawebinar.topjava.TestUtil;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+import ru.javawebinar.topjava.web.json.JsonUtil;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+class MealRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = MealRestController.REST_URL + '/';
+
+    @Test
+    void testCreate() throws Exception {
+        Meal created = getCreated();
+        ResultActions action = mockMvc.perform(post(REST_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(JsonUtil.writeValue(created)))
+            .andExpect(status().isCreated());
+
+        Meal returned = TestUtil.readFromJson(action, Meal.class);
+        created.setId(returned.getId());
+
+        assertMatch(returned, created);
+        assertMatch(mealService.getAll(USER_ID), created, MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1);
+    }
+
+    @Test
+    void testGet() throws Exception {
+        mockMvc.perform(get(REST_URL + MEAL1_ID))
+            .andExpect(status().isOk())
+            .andDo(print())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(contentJson(MEAL1));
+    }
+
+    @Test
+    void testUpdate() throws Exception {
+        Meal updated = getUpdated();
+        mockMvc.perform(put(REST_URL + MEAL1_ID)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(JsonUtil.writeValue(updated)))
+            .andExpect(status().isOk());
+        assertMatch(mealService.get(MEAL1_ID, USER_ID), updated);
+    }
+
+    @Test
+    void testDelete() throws Exception {
+        mockMvc.perform(delete(REST_URL + MEAL1_ID))
+            .andDo(print())
+            .andExpect(status().isNoContent());
+        assertMatch(mealService.getAll(USER_ID), MEAL6, MEAL5, MEAL4, MEAL3, MEAL2);
+    }
+
+    @Test
+    void testGetAll() throws Exception {
+        TestUtil.print(mockMvc.perform(get(REST_URL))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(contentJson(MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1)));
+    }
+
+    @Test
+    void testGetBetween() throws Exception {
+        mockMvc.perform(get(REST_URL + "between")
+            .param("startLocalDateTime", "2015-05-30T01:00:00")
+            .param("endLocalDateTime", "2015-05-30T23:00:00"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(contentJson(MEAL3, MEAL2, MEAL1));
+    }
+}
